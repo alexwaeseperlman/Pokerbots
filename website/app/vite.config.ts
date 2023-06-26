63b828b8a67d077a39e3407f84e5da55a3555d90
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2023-06-25 17:05:44 -0700

    Create dev compose file

--- website/app/vite.config.ts
+++ website/app/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.BACKEND_ADDRESS ?? "http://localhost:3000",
         changeOrigin: true,
         secure: false,
         ws: true,
