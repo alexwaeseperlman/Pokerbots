35fc6cd374b6b82e407f5c463e0cf8df4861aa3c
Alex <16113308+alexwaeseperlman@users.noreply.github.com>
2023-07-03 13:25:44 -0700

    add 404 page (#37)

---
+++ website/app/src/NotFound/index.tsx
@@ -0,0 +1,51 @@
+import { Box, Typography } from "@mui/material";
+import graphic from "./graphic.png";
+import React from "react";
+import { primary_background } from "../styles.module.css";
+
+export default function NotFound() {
+  return (
+    <Box
+      className={`${primary_background}`}
+      sx={{
+        width: "100%",
+        color: "white",
+        p: 4,
+        pb: 16,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        flexGrow: 1,
+      }}
+    >
+      <Box
+        sx={{
+          backgroundImage: `url(${graphic})`,
+          filter: "grayscale(100%)",
+          opacity: 0.4,
+          backgroundPosition: "center",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          mixBlendMode: "screen",
+          width: "100%",
+          maxWidth: "100vw",
+          height: "100%",
+          display: "block",
+          overflow: "hidden",
+          pointerEvents: "none",
+        }}
+      ></Box>
+      <Box
+        sx={{
+          zIndex: 1,
+        }}
+      >
+        <Typography variant="h2">There is no page at this address.</Typography>
+      </Box>
+    </Box>
+  );
+}
