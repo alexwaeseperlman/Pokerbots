c0477e4aa3f3ac30968e4863d82690e0408ab3f3
Jun Kai Liao <44240472+laheau@users.noreply.github.com>
2023-08-20 18:49:41 -0400

    Sandbox (#65)
    * Log upload (#40)
    
    * First draft of log uploading
    
    * Fix deployment
    
    * Ensure logs get uploaded
    
    * Add api endpoint for game logs
    
    * Add api endpoint for bot build logs
    
    * Remove process library
    
    * Add download button to site
    
    * Fix build log on compile error
    
    * Download private log by default
    
    * bwrap command
    
    * added bwrap
    
    * fix compiler error and remove bwrap.sh
    
    * added sandboxing for gameplay
    
    * cpu quota implemented not tested
    
    * killing process is semi working
    
    * fixed kill of bwrap
    
    * remove unused library
    
    * remove unused functions
    
    * added init to builder and gameplay
    
    * rename 'challengerucket' to 'bot_bucket' lol
    
    * Update github actions
    
    * Fix setup-env calls
    
    ---------
    
    Co-authored-by: Alex <16113308+alexwaeseperlman@users.noreply.github.com>
    Co-authored-by: Alex Waese-Perlman <alexwaeseperlman@gmail.com>

--- workers/builder/src/main.rs
+++ workers/builder/src/main.rs
@@ -9,11 +9,23 @@ async fn process(
     s3: &aws_sdk_s3::Client,
     reqwest_client: &reqwest::Client,
 ) -> Result<(), Box<dyn Error>> {
-    let challengerucket = std::env::var("BOT_S3_BUCKET")?;
-    let compiled_challengerucket = std::env::var("COMPILED_BOT_S3_BUCKET")?;
+    let bot_bucket = std::env::var("BOT_S3_BUCKET")?;
+    let compiled_bot_bucket = std::env::var("COMPILED_BOT_S3_BUCKET")?;
     fs::create_dir_all(format!("/tmp/{}", bot)).await?;
     let bot_path = std::path::Path::new("/tmp").join(&bot);
-    shared::s3::download_file(&bot, &bot_path.join("bot.zip"), &challengerucket, &s3).await?;
+    // Command::new("mount")
+    //     .arg("-t")
+    //     .arg("tmpfs")
+    //     .arg("-o")
+    //     .arg("rw,size=2G")
+    //     .arg(format!("{}", bot))
+    //     .arg(format!("/tmp/{}", bot))
+    //     .stderr(Stdio::null())
+    //     .stdout(Stdio::null())
+    //     .current_dir(format!("/tmp/{}", bot))
+    //     .status()
+    //     .await?;
+    shared::s3::download_file(&bot, &bot_path.join("bot.zip"), &bot_bucket, &s3).await?;
     let result = build_bot(bot_path).await;
     // upload the logs
     let log = fs::read(format!("/tmp/{}/logs", bot)).await?;
@@ -45,7 +57,7 @@ async fn process(
     // TODO: this should use a presigned url, like the logs
     if let Err(e) = s3
         .put_object()
-        .bucket(compiled_challengerucket)
+        .bucket(compiled_bot_bucket)
         .key(format!("{}", &bot))
         .body(
             fs::read(format!("/tmp/{}/compiled_bot.zip", &bot))
