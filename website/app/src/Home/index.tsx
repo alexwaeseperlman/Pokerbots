276c0eb274106c52142aae6e9303a95abf957f8b
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2023-12-26 16:10:30 -0500

    Add animation to front page

--- website/app/src/Home/index.tsx
+++ website/app/src/Home/index.tsx
@@ -9,24 +9,45 @@ import graphic from "./graphic.png";
 import BackgroundImage from "../components/BackgroundImage";
 import HeaderFooter from "../components/HeaderFooter";
 
-export function LogoText({text}: { text: string }) {
-  let vals = '0px 0px';
-  for (let i = 0; i <= 20; i++) vals += `, ${i/2}px ${i/2}px`;
-  console.log(vals);
-  return <Typography level="h1" fontSize={100} color='inherit' sx={{
-    position:'relative',
-    letterSpacing: '10px',
-    '&:before': {
-      content: `"${text}"`,
-      position:'absolute',
-      zIndex: -1,
-      top:0,
-      left:0,
-      textShadow: vals,
-      color: '#CDC0FF',
-      mask: 'repeating-linear-gradient(45deg, transparent 0 3px, rgba(0,0,0,0.5) 0 6px)'
-    },
-  }}>{text}</Typography>
+export function LogoText({ text }: { text: string }) {
+  let vals1 = "0px 0px";
+  for (let i = 0; i <= 20; i++) vals1 += `, ${i / 2}px ${i / 2}px`;
+  let vals2 = "0px 0px";
+  for (let i = 0; i <= 20; i++) vals2 += `, -${i / 2}px -${i / 2}px`;
+  console.log(vals2);
+  return (
+    <Typography
+      level="h1"
+      color="inherit"
+      sx={theme => ({
+        fontSize: 100,
+        [theme.breakpoints.down("sm")]: {
+          fontSize: 88,
+          letterSpacing: "5px",
+        },
+        position: "relative",
+        letterSpacing: "10px",
+        userSelect: "none",
+
+        "&:before": {
+          content: `"${text}"`,
+          position: "absolute",
+          zIndex: -1,
+          top: 0,
+          left: 0,
+          textShadow: vals1,
+          color: "#CDC0FF",
+          mask: "repeating-linear-gradient(45deg, transparent 0 3px, rgba(0,0,0,0.5) 0 6px)",
+          transition: "text-shadow 0.2s ease-out",
+        },
+        "&:hover:before": {
+          textShadow: vals2
+        },
+      })}
+    >
+      {text}
+    </Typography>
+  );
 }
 
 export default function HomePage() {
@@ -40,7 +61,7 @@ export default function HomePage() {
           flexDirection: "column",
           justifyContent: "center",
           display: "flex",
-          pb: 4
+          pb: 4,
         }}
       >
         <Box
@@ -60,7 +81,7 @@ export default function HomePage() {
               alignItems: "left",
             }}
           >
-            <LogoText text="UPAC"/>
+            <LogoText text="UPAC" />
           </Box>
           <Box
             sx={{
@@ -82,7 +103,8 @@ export default function HomePage() {
           </Box>
           <Typography textColor="inherit" level="body-md">
             The competition will start in 2024. For sponsorship inquiries,
-            please contact <Link href="mailto:alex@alexwp.com">alex@alexwp.com</Link>
+            please contact{" "}
+            <Link href="mailto:alex@alexwp.com">alex@alexwp.com</Link>
           </Typography>
         </Box>
       </Container>
